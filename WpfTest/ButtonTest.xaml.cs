b9b209cb88cab88d46fd0979cbf5bbb866383307
ApogeeX <emil.palczynski@gmail.com>
2023-04-01 13:04:06 +0200

    TextBlock

---
+++ WpfTest/ButtonTest.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfTest
+{
+    /// <summary>
+    /// Interaction logic for ButtonTest.xaml
+    /// </summary>
+    public partial class ButtonTest : Window
+    {
+        public ButtonTest()
+        {
+            InitializeComponent();
+            BtnTest.Content = "ABC";
+            BtnTest.Click += BtnTest_Click;
+        }
+
+        private void BtnTest_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("click!");
+        }
+    }
+}
